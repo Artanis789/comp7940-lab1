cae10845a1a773a9a0a302a7b936867ce8ad159f
Artanis789 <22426744@life.hkbu.edu.hk>
2023-02-18 02:24:15 +0800

    Third commit

--- chatbot.py
+++ chatbot.py
@@ -33,6 +33,7 @@ def main():
 # on different commands - answer in Telegram
     dispatcher.add_handler(CommandHandler("add", add))
     dispatcher.add_handler(CommandHandler("help", help_command))
+    dispatcher.add_handler(CommandHandler("hello", hello_Kevin))
 # To start the bot:
     updater.start_polling()
     updater.idle()
@@ -57,6 +58,17 @@ def add(update: Update, context: CallbackContext) -> None:
         update.message.reply_text('You have said ' + msg + ' for ' + redis1.get(msg).decode('UTF-8') + ' times.')
     except (IndexError, ValueError):
         update.message.reply_text('Usage: /add <keyword>')
+
+def hello_Kevin(update: Update, context: CallbackContext) -> None:
+    """Send a message when the command /hello is issued."""
+    try:
+        global redis1
+        logging.info(context.args[0])
+        msg = context.args[0] # /add keyword <-- this should store the keyword
+        redis1.incr(msg)
+        update.message.reply_text('Good day,' + msg + '!')
+    except (IndexError, ValueError):
+        update.message.reply_text('Usage: /hello <keyword>')
 
 if __name__ == '__main__':
     main()
