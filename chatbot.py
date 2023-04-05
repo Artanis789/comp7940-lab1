38f50878d89fb10a3547d96d4d62635991020612
Robin <robin@example.com>
2023-04-06 01:08:30 +0800

    crud cook menu and add new image generator robot

--- chatbot.py
+++ chatbot.py
@@ -5,6 +5,8 @@ import openai
 import mysql.connector
 import os
 import logging
+from wrapt_timeout_decorator import *
+import time
 
 db_config = {
     'host': os.environ['DB_HOST'],
@@ -24,45 +26,42 @@ def main():
     logging.info(f"API-KEY = {os.environ['GPT_API']}")
 
     # register a dispatcher to handle message: here we register an echo dispatcher
-    echo_handler = MessageHandler(Filters.text & (~Filters.command), echo)
+    gpt_handler = MessageHandler(Filters.text & (~Filters.command), gpt_reply)
 
-    dispatcher.add_handler(echo_handler)
+    dispatcher.add_handler(gpt_handler)
     # on different commands - answer in Telegram
     dispatcher.add_handler(CommandHandler("help", help_command))
     dispatcher.add_handler(CommandHandler("cook", cook))
-    dispatcher.add_handler(CommandHandler("list", cook_list))
-    dispatcher.add_handler(CommandHandler("add", cook_add))
-    dispatcher.add_handler(CommandHandler("ai", gpt_reply))
+    dispatcher.add_handler(CommandHandler("menu", cook_list))
+    dispatcher.add_handler(CommandHandler("menu_add", cook_add))
+    dispatcher.add_handler(CommandHandler("menu_del", cook_del))
+    dispatcher.add_handler(CommandHandler("menu_update", cook_update))
+    dispatcher.add_handler(CommandHandler("image", image_reply))
 
     # To start the bot:
     updater.start_polling()
     updater.idle()
 
-
-def echo(update: Update, context: CallbackContext):
-    reply_message = update.message.text.upper()
-    logging.info("Update: " + str(update))
-    logging.info("context: " + str(context))
-    context.bot.send_message(
-        chat_id=update.effective_chat.id, text=reply_message)
-
-# Define a few command handlers. These usually take the two arguments update and
-# context. Error handlers also receive the raised TelegramError object in error.
-
-
+@timeout(10)
 def help_command(update: Update, context: CallbackContext) -> None:
     """Send a message when the command /help is issued."""
 
     help_message = "Hello, I'm a smart chatbot powered by ChatGPT, You can talk to me about anything.\n" +\
         "I also prepare some interesting commands for you to learn how to cook:\n\n" +\
         "/cook: Enter the name of the dish you are interested in, I'll give you a video.\n\n" +\
-        "/list: list the menu.\n\n" +\
-        "/add: Enter the name and the video site of the dish to add new item to the menu.\n" +\
-        "Example: /add 鱼香肉丝 https://www.bilibili.com/video/BV1KT4y1Y78z/"
+        "/menu: list the menu.\n\n" +\
+        "/menu_add: Enter the name and the video site of the dish to add new item to the menu.\n" +\
+        "Example: /menu_add 鱼香肉丝 https://www.cook-video/example\n\n" +\
+        "/menu_del: Enter the name of the dish to delete it from the menu.\n" +\
+        "Example: /menu_del 鱼香肉丝\n\n" +\
+        "/menu_update: Enter the name and a new url to update a dish.\n" +\
+        "Example: /menu_update 鱼香肉丝 https://www.new-cook-video/example\n\n" +\
+        "/image: Enter a prompt, I can generate a realistic image for you\n" +\
+        "Example: /image a lovely cat"
 
     update.message.reply_text(help_message)
 
-
+@timeout(10)
 def cook(update: Update, context: CallbackContext) -> None:
     cook_name = context.args[0]
     db = mysql.connector.connect(**db_config)
@@ -79,6 +78,7 @@ def cook(update: Update, context: CallbackContext) -> None:
         update.message.reply_text(
             f"I'm sorry, I don't know how to cook {cook_name}...")
 
+@timeout(10)
 def cook_list(update: Update, context: CallbackContext) -> None:
     db = mysql.connector.connect(**db_config)
     cursor = db.cursor()
@@ -93,6 +93,7 @@ def cook_list(update: Update, context: CallbackContext) -> None:
 
     update.message.reply_text(result)
 
+@timeout(10)
 def cook_add(update: Update, context: CallbackContext):
     if len(context.args) < 2:
         update.message.reply_text("To add new item, please enter name and url of the video.")
@@ -112,29 +113,99 @@ def cook_add(update: Update, context: CallbackContext):
             update.message.reply_text("Successfully add a new item to menu!")
         except Exception as e:
             logging.debug(e)
-            update.message.reply_text('Fail to add new item to menu, please try again')
+            update.message.reply_text('Failed to add new item to menu, please try again')
 
+@timeout(10)
+def cook_del(update: Update, context: CallbackContext):
+    if len(context.args) < 1:
+        update.message.reply_text("To delete an item, please enter the name of it.")
+    else:
+        name = context.args[0]
+        sql = f'DELETE FROM cooking_list WHERE name="{name}";'
+        logging.info(f'\n{sql}')
+
+        db = mysql.connector.connect(**db_config)
+        cursor = db.cursor()
+        try:
+            cursor.execute(sql)
+            db.commit()
+            cursor.close()
+            db.close()
+            update.message.reply_text("Successfully delete an item from menu!")
+        except Exception as e:
+            logging.debug(e)
+            update.message.reply_text('Failed to delete an item, please try again!')
+
+@timeout(10)
+def cook_update(update: Update, context: CallbackContext):
+    if len(context.args) < 2:
+        update.message.reply_text("To update an item, please enter the name of the item and a new url.")
+    else:
+        name = context.args[0]
+        url = context.args[1]
+        sql = f'UPDATE cooking_list SET url = "{url}" WHERE name = "{name}";'
+        logging.info(f'\n{sql}')
+
+        db = mysql.connector.connect(**db_config)
+        cursor = db.cursor()
+        try:
+            cursor.execute(sql)
+            db.commit()
+            cursor.close()
+            db.close()
+            update.message.reply_text("Successfully update an item from menu!")
+        except Exception as e:
+            logging.debug(e)
+            update.message.reply_text('Failed to update an item, please try again!')
+
+@timeout(60)
+def chat(msg):
+    response = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "user", "content": msg},
+        ]
+    )
+    return response
 
 def gpt_reply(update: Update, context: CallbackContext):
     try:
-        msg = " ".join(context.args)
+        msg = update.message.text
         logging.info(msg)
-
-        response = openai.ChatCompletion.create(
-            model="gpt-3.5-turbo",
-            messages=[
-                {"role": "user", "content": msg},
-            ]
-        )
+        start = time.time()
+        response = chat(msg)
+        logging.info(f"Request cost {time.time() - start}seconds")
         result = ""
         for choice in response.choices:
             result += choice.message.content
         update.message.reply_text(result)
 
     except Exception as e:
-        logging.debug(str(e))
-        update.message.reply_text("Something wrong with chatgpt!")
+        logging.info(str(e))
+        update.message.reply_text(f"Something wrong with chatbot, please retry!")
 
+@timeout(60)
+def image(prompt):
+    response = openai.Image.create(
+        prompt=prompt,
+        n=1,
+        size="1024x1024"
+    )
+    image_url = response['data'][0]['url']
+    return image_url
+
+def image_reply(update: Update, context: CallbackContext):
+    try:
+        prompt = " ".join(context.args)
+        logging.info(prompt)
+        start = time.time()
+        image_url = image(prompt)
+        logging.info(f"Request cost {time.time() - start}seconds")
+        update.message.reply_text(f'Here is the url of generated image:\n{image_url}')
+
+    except Exception as e:
+        logging.info(str(e))
+        update.message.reply_text(f"Something wrong with chatbot, please retry!")
 
 if __name__ == "__main__":
     main()
--- dockerfile
+++ dockerfile
@@ -9,7 +9,7 @@ RUN pip install -r requirements.txt
 COPY chatbot.py /
 
 ENV ACCESS_TOKEN    6021929462:AAGu_0F6w5Zk2o0t0pcKknm8E0KECLBaOo8
-ENV GPT_API         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
+ENV GPT_API         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
 ENV DB_HOST         cook.mysql.database.azure.com
 ENV DB_USER         comp7940
 ENV DB_PWD          project7940!
